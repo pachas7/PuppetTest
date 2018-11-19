04c8be0bc5b842bbc95169b2cfff76b217f4ad9b
pachas7 <josedavid.b77@gmail.com>
2018-11-19 10:28:10 +0100

    Create site.pp

---
+++ code/manifests/site.pp
@@ -0,0 +1,11 @@
+node default {
+
+    file {'/etc/motd':
+        content => 'This is my testing content',
+        }
+    
+    service {'postfix':
+        ensure => 'stopped',
+        enable => 'false',
+        }
+}
